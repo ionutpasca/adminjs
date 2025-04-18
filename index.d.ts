ab2c399550f8c26c3c9ce2df522fc1665021d58f
Ionut Pasca <pascaionut94@gmail.com>
2025-04-18 15:50:47 +0300

    Update json exports

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,9 @@ import AdminJS from './types/src/index.js'
 
 import { ReduxState } from './types/src/frontend/store/store.js'
 
-export * from './types/src/index.js'
+export * from './lib/index.js'
 
-export {
-  AdminJS,
-  ReduxState,
-}
+export { AdminJS, ReduxState }
 
 export default AdminJS
 
